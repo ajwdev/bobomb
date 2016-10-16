ed03bb6ee4a2c37b41830969ed86f507ed5ce010
Andrew Williams <williams.andrew@gmail.com>
2016-10-16 15:10:24 -0500

    Add bne instruction

--- src/cpu/cpu.rs
+++ src/cpu/cpu.rs
@@ -190,6 +190,12 @@ impl Cpu {
                 let word = self.read_word_and_increment();
                 if self.SR.Zero { self.move_pc_relative(word); }
             }
+            // BNE (branch on non zero result)
+            0xd0 => {
+                // TODO Dry this up with beq
+                let word = self.read_word_and_increment();
+                if !self.SR.Zero { self.move_pc_relative(word); }
+            }
             // AND # immediate
             0x29 => {
                 let word = self.read_word_and_increment();
@@ -414,6 +420,40 @@ mod test {
         let mut cpu = mock_cpu(&[0xf0, 0x82]); // hex 0x82 is signed -126
 
         cpu.SR.Zero = true;
+        assert!(cpu.PC == 0x8000, "expected 0x8000, got {:#x}", cpu.PC);
+        cpu.execute_instruction(); // Two byte instruction so *substract* two bytes below
+
+        // NOTE For posterity, this actually drops us below the ROM
+        // start range which I dont think will happen with real ROMs.
+        // This should be fine for our test though.
+        assert!(cpu.PC == 0x7f84, "expected 0x7f82, got {:#x}", cpu.PC);
+    }
+
+    #[test]
+    fn test_bne_skip() {
+        let mut cpu = mock_cpu(&[0xd0, 0xff]);
+
+        cpu.SR.Zero = true;
+        assert!(cpu.PC == 0x8000, "expected 0x8000, got {:#x}", cpu.PC);
+        cpu.execute_instruction();
+        assert!(cpu.PC == 0x8002, "expected 0x8002, got {:#x}", cpu.PC);
+    }
+
+    #[test]
+    fn test_bne_take_positive() {
+        let mut cpu = mock_cpu(&[0xd0, 0x2a]);
+
+        cpu.SR.Zero = false;
+        assert!(cpu.PC == 0x8000, "expected 0x8000, got {:#x}", cpu.PC);
+        cpu.execute_instruction(); // Two byte instruction so *add* two below
+        assert!(cpu.PC == 0x802c, "expected 0x802a, got {:#x}", cpu.PC);
+    }
+
+    #[test]
+    fn test_bne_take_negative() {
+        let mut cpu = mock_cpu(&[0xd0, 0x82]); // hex 0x82 is signed -126
+
+        cpu.SR.Zero = false;
         assert!(cpu.PC == 0x8000, "expected 0x8000, got {:#x}", cpu.PC);
         cpu.execute_instruction(); // Two byte instruction so *substract* two bytes below
 
