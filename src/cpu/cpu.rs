f1b898164c0446196855ea1b176ccfc353a2019a
Andrew Williams <williams.andrew@gmail.com>
2016-10-16 23:31:02 -0500

    Add JSR instruction

--- src/cpu/cpu.rs
+++ src/cpu/cpu.rs
@@ -178,6 +178,7 @@ impl Cpu {
 
     // NOTE 6502 stack grows downward
     fn push_stack(&mut self, word: u8) {
+        // TODO Panic if pointer ends up in a page besides page 1
         let ptr = STACK_START + self.SP as u16;
         self.mem.write_word(ptr, word);
         self.SP -= 1;
@@ -185,6 +186,7 @@ impl Cpu {
     }
 
     fn pop_stack(&mut self) -> u8 {
+        // TODO Panic if pointer ends up in a page besides page 1
         self.SP += 1;
         self.stack_depth -= 1;
         let ptr = STACK_START + self.SP as u16;
@@ -230,6 +232,21 @@ impl Cpu {
                 // TODO Dry this up with beq
                 let word = self.read_word_and_increment();
                 if !self.SR.Zero { self.move_pc_relative(word); }
+            }
+            0x20 => {
+                // https://wiki.nesdev.com/w/index.php/RTS_Trick#About_JSR_and_RTS
+                let addr = self.read_dword_and_increment();
+                // PC is now at the next instruction. According to the doc above we are to
+                // take this value and subtract one from it, THEN push it on the stack. On pop
+                // we then add 1 to the address. I'm not sure why we just cant push the current PC
+                // but there is probably a reason.
+                let ret = self.PC - 1;
+
+                // push the high byte and then the low byte
+                self.push_stack(((ret & 0xFF00) >> 8) as u8);
+                self.push_stack((ret & 0x00FF) as u8);
+
+                self.PC = addr;
             }
             // AND # immediate
             0x29 => {
@@ -379,6 +396,7 @@ mod test {
 
     #[test]
     fn test_zero_page_address() {
+        // XXX Is this really testing anything?
         assert!(Cpu::zero_page_address(0xff) == 0x00ff);
         assert!(Cpu::zero_page_address(0x00) == 0x0000);
         assert!(Cpu::zero_page_address(0x12) == 0x0012);
@@ -437,6 +455,22 @@ mod test {
 
     // Instruction tests
     //
+
+    #[test]
+    fn test_jsr() {
+        let mut cpu = mock_cpu(&[0x20, 0xef, 0xbe]);
+
+        assert!(cpu.PC == 0x8000, "expected 0x8000, got {:#x}", cpu.PC);
+
+        cpu.execute_instruction();
+        assert!(cpu.PC == 0xbeef, "expected 0xbeef, got {:#x}", cpu.PC);
+
+        cpu.debug_stack();
+        let mut result = cpu.pop_stack();
+        assert!(result == 0x02, "expected 0x02, got {:#x}", result);
+        result = cpu.pop_stack();
+        assert!(result == 0x80, "expected 0x80, got {:#x}", result);
+    }
 
     #[test]
     fn test_bpl_skip() {
