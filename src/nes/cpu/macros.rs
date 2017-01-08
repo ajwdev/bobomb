2be3fbefee4983b8391bca9764a6c8524d54ff45
Andrew Williams <williams.andrew@gmail.com>
2017-01-08 13:16:36 -0600

    Fix typo in assert reset macro

--- src/nes/cpu/macros.rs
+++ src/nes/cpu/macros.rs
@@ -8,7 +8,7 @@ macro_rules! assert_status_set {
 
 macro_rules! assert_status_reset {
     ($c:expr, $f:expr) => {
-        assert!(!$c.SR.is_set($f), "expected '{:?}' status register to be set", $f);
+        assert!(!$c.SR.is_set($f), "expected '{:?}' status register to be reset", $f);
     }
 }
 
