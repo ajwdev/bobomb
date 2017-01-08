f47eccc141132061cad2fda0a6635413d38c4c1e
Andrew Williams <williams.andrew@gmail.com>
2017-01-08 13:40:42 -0600

    Improve tests in ROR instruction

--- src/nes/cpu/opcodes/ror.rs
+++ src/nes/cpu/opcodes/ror.rs
@@ -45,15 +45,46 @@ mod test {
     #[test]
     fn test_eor() {
         let mut cpu = mock_cpu(&[0x66,0xFF]);
+
         cpu.mem.write_word(0x00FF, 0b10000000);
         cpu.SR.set(Flags::Carry);
-
-        // TODO test zero and negative status
+        cpu.SR.reset(Flags::Zero);
+        cpu.SR.reset(Flags::Negative);
 
         cpu.execute_instruction();
         let result = cpu.mem.read_word(0x00FF);
         assert_equalx!(0b11000000, result);
         assert_status_reset!(cpu, Flags::Carry);
+        assert_status_reset!(cpu, Flags::Zero);
+        assert_status_set!(cpu, Flags::Negative);
+
+
+        cpu.rewind();
+        cpu.mem.write_word(0x00FF, 0b10000001);
+        cpu.SR.reset(Flags::Carry);
+        cpu.SR.reset(Flags::Zero);
+        cpu.SR.reset(Flags::Negative);
+
+        cpu.execute_instruction();
+        let result = cpu.mem.read_word(0x00FF);
+        assert_equalx!(0b01000000, result);
+        assert_status_set!(cpu, Flags::Carry);
+        assert_status_reset!(cpu, Flags::Zero);
+        assert_status_reset!(cpu, Flags::Negative);
+
+
+        cpu.rewind();
+        cpu.mem.write_word(0x00FF, 0b00000001);
+        cpu.SR.reset(Flags::Carry);
+        cpu.SR.reset(Flags::Zero);
+        cpu.SR.reset(Flags::Negative);
+
+        cpu.execute_instruction();
+        let result = cpu.mem.read_word(0x00FF);
+        assert_equalx!(0b00000000, result);
+        assert_status_set!(cpu, Flags::Carry);
+        assert_status_set!(cpu, Flags::Zero);
+        assert_status_reset!(cpu, Flags::Negative);
     }
 }
 
